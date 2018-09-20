9a65b1904e4830e2c64811188870453ebba7be54
Daniel <hoptond848@protonmail.com>
2018-09-20 13:32:59 +0100

    laid out basic html for index.php and account.php

---
+++ account.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Account Page</title>
+</head>
+<body>
+    <p>You are now logged in. Look at all this cool account stuff you can see:</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Welcome</title>
+    </head>
+    <body>
+        <form method="post">
+            <label>Username:</label>
+            <input type="text" name="username" title="username">
+            <label>Password:</label>
+            <input type="text" name="password" title="username">
+            <button name ="submit">Submit</button>
+        </form>
+    <p>todo: add login message here</p>
+    </body>
+</html>
